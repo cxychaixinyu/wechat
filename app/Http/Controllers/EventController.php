d77b4b1a20e075a072ec146ab05fd840b9d7c65f
cxychaixinyu <1612418369@qq.com>
2019-09-27 10:38:30 +0800

    ll

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -116,7 +116,8 @@ class EventController extends Controller
                     'add_time'=>time()
                 ]);
             }
-            $message = '欢迎'.$user_info['nickname'].'同学，感谢您的关注';
+            $message = '欢迎，感谢您的关注';
+//            '.$user_info['nickname'].'同学
             $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
             echo $xml_str;
         }
